f0046b6dc96302e9f6cf1e2e1e440d753faf9fdb
Marie T <marie.toupuissant@gmail.com>
2022-09-01 15:56:55 +0200

    First message with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `Bienvenue sur le site de Marie`
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myFirstApp';
